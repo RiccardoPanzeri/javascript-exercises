7793fe63e518a9b802ee6018f92a3141b24afa63
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-06-28 21:25:03 +0200

    Completa esercizio 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let counter = 0;
+    let output = "";
+    if(times < 0){
+        return "ERROR";
+    }
+    while(counter < times){
+        output += string;
+        counter++;
+    }
+    return output;
 };
 
 // Do not edit below this line
