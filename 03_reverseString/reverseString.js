120f9eab9c78f1a27651bdb1fcadf7c022dde629
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-06-28 21:48:49 +0200

    Completa esercizio 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringa) {
+    const vettore = stringa.split("");
+    let output = "";
+    for( let i = -1; i >= -(vettore.length); i--){
+        output += vettore.at(i); 
 
+    }
+    return output;
 };
 
 // Do not edit below this line
