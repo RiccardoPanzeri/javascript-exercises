90b545397ab2367cb92120d30d8ae5433bfc4a3d
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-06-28 22:38:22 +0200

    Completa esercizio 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valori) {
+    let output = [];
+    let presente = false;
+    for(let x of array){
+        for(let y of valori){
+            if(x === y){
+                presente = true;
+                break;
+            }
+        }
+        if(!presente){
+            output.push(x);
+            
+        }
+        presente = false;
+    }
+    return output;
 };
 
 // Do not edit below this line
