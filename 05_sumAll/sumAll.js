c01541f7418315577477b0090dbc6c116e62271e
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-06-28 22:58:47 +0200

    Completa esercizio 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(rangeBegin, rangeEnd) {
+    let total = 0;
+    let begin;
+    let end;
+    if(rangeBegin < 0 || rangeEnd < 0 || typeof rangeEnd === "string"|| typeof rangeBegin === "string" ||isNaN(rangeBegin) 
+        ||isNaN(rangeEnd)){
+        return "ERROR";
+    }
+    if(rangeBegin > rangeEnd){
+        begin = rangeEnd;
+        end = rangeBegin;
+    }else{
+        end = rangeEnd;
+        begin = rangeBegin;
+    }
+    for(let i = begin; i <= end; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
