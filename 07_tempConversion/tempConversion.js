09d807acca1b448e97415e50a49fe2c48324eccd
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-06-29 01:29:32 +0200

    Completa esercizio 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitValue) {
+      
+      let result =  ((farenheitValue -32) * (5/9));
+      if(Number.isInteger(result)){
+        return result
+      }else{
+        return Number(result.toFixed(1));
+      }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CelsiusValue) {
+    let result = (CelsiusValue * (9/5)) + 32;
+    if(Number.isInteger(result)){
+      return result;
+    }else{
+      return Number(result.toFixed(1));
+    }
 };
 
 // Do not edit below this line
