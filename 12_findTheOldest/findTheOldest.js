be656441ce07a485d1c7d0c471fdf58f30c9cccc
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-06-29 03:42:28 +0200

    Completa esercizio 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldest = array[0];
+    let temp = 0;
+    let age = 0;
+    let now = new Date();
+    for(let element of array){
+        if(element.yearOfDeath === undefined){
+            age = now.getFullYear()- element.yearOfBirth;
+            console.log("age undefined : " + age + now.getFullYear());
+        }else{
+            age = element.yearOfDeath - element.yearOfBirth;
+            console.log("age defined : " + age);
+        }
+        if(age > temp){
+            temp = age;
+            oldest = element;
+            console.log(temp);
+        }
+        
+    }
 
+    return oldest;
 };
 
 // Do not edit below this line
