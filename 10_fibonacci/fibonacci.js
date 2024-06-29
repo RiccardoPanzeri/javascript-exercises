5048cc943186af38632c7d4f4ecbd7d93c90722c
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-06-29 02:59:27 +0200

    Completa esercizio 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    if (value == 0){
+        return 0;
+    }else if(value < 0){
+        return "OOPS";
+    }else if(value == 1 ||value == 2){
+        return 1;
+    }
+    let result = 2;
+    let preceding = result - 1;
+    let temp = 0;
+    let counter = 3;
+    while(counter < value){
+        temp = result;
+        result += (preceding);
+        preceding = temp;
+        counter++;
+    }
+
+    return result;
 
 };
 
