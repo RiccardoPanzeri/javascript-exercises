471d921db842707e755c49355a5436ed8f9149a0
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-06-29 02:35:24 +0200

    Completa esercizio 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const { lowerCase } = require("case-anything");
 
+const palindromes = function (string) {
+    string = string.replaceAll("!", "");
+    string = string.replaceAll(",", "");
+    string = string.replaceAll(".", "");
+    string = string.replaceAll(" ", "");
+    string = lowerCase(string);
+    const array = string.split("");
+    let reverseString = "";
+    array.reverse();
+    array.forEach(element => {
+        reverseString += element;
+        
+    });
+    console.log(reverseString);
+    console.log(string);
+    if(reverseString === string){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
