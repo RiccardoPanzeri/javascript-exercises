0d26d2b7acfb5910b232e09edba6af96d5b3683a
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-06-29 02:09:32 +0200

    Completa esercizio 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
+const add = function(val1, val2) {
+  let result = val1 + val2;
+  
+  return result;
 	
 };
 
-const subtract = function() {
+const subtract = function(value1, ...values) {
+  let result = value1;
+  for(let value of values){
+    result -= value;
+  }
+  return result;
 	
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+	let result = 0;
+  for(let value of values){
+    
+    result += value;
+    
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(values) {
+  let result = 1;
+  for(let value of values){
+    result *= value;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+    return base ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+    let result = val;
+  if(val == 1 || val == 0){
+    return 1;
+  }else{
+    result *= factorial(val - 1);
+  }
+  return result;
 };
 
 // Do not edit below this line
